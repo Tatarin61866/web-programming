64c9b289a4e3a8592535ef7bd4830fbf2ad490cd
TataRin <124790207+Tatarin61866@users.noreply.github.com>
2023-10-30 14:41:03 +0300

    Add files via upload

--- lab09/app.py
+++ lab09/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask('lab09')
 
@@ -18,4 +18,14 @@ def page2():
 	
 @app.route('/p3')
 def page3():
+	return render_template('page3.html')
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+# 	if request.method == 'POST':
+# 		value = request.form.get('chk')
+# 		if value == empty:
+# 			flash('checkbox is empty')
+# 			return  redirect ('/p2')
 	return render_template('page3.html')
